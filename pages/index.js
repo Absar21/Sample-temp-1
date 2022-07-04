15ac4005eecf3fd8eb19697c1990ab3c4acde230
Absar21 <107810382+Absar21@users.noreply.github.com>
2022-07-04 19:35:34 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Script from 'next/script';
-import { Html,  Main, NextScript } from 'next/document'
+
 
 
 export default function Home() {
